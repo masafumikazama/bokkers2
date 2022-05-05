64dc86fedcd7f67e91b9cfbad1286849089c61c1
masafumikazama <masanomaildesu@yahoo.co.jp>
2022-05-05 17:44:08 +1000

    [Finish]setup_app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = current_user
+    @books = @user.books
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to user_path
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
 end
